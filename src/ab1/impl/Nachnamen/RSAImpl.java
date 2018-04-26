36899406e6674157f535c16129675b392f667517
IIIlllllIlIllIllI <rtosir@outlook.com>
2018-04-26 11:49:44 +0200

    init

--- src/ab1/impl/Nachnamen/RSAImpl.java
+++ src/ab1/impl/Nachnamen/RSAImpl.java
@@ -4,8 +4,8 @@ import java.math.BigInteger;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
+import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Random;
 
 import ab1.RSA;
 
@@ -35,10 +35,7 @@ public class RSAImpl implements RSA {
 		BigInteger phi = q.subtract(BigInteger.ONE).multiply(p.subtract(BigInteger.ONE));
 		// e= 2^16+1
 		BigInteger e = new BigInteger("65537");
-		// e=e.pow(16).add(BigInteger.ONE);
-		while (!e.gcd(phi).equals(BigInteger.ONE) || e.equals(BigInteger.ONE) || e.equals(BigInteger.ZERO)) {
-			e = e.add(new BigInteger("2"));
-		}
+
 		BigInteger d = e.modInverse(phi);
 		publicKey = new PublicKey(nn, e);
 		privateKey = new PrivateKey(nn, d);
@@ -57,12 +54,37 @@ public class RSAImpl implements RSA {
 
 	@Override
 	public byte[] encrypt(byte[] data, boolean activateOAEP) {
-		byte[] result=new byte[128*(data.length/128+1)];
+		ArrayList<Byte> al=new ArrayList<>();
+		byte[] result;
 		for (int i = 0; i < data.length; i+=127) {
-			byte[] tmp = new byte[127];
-		    System.arraycopy(data, i, tmp, 0, tmp.length<=data.length-i? tmp.length:data.length-i);
+			byte[] tmp = new byte[128];
+			int lenLastBlock=data.length-i;
+			if(!(127>lenLastBlock)){
+				lenLastBlock=127;
+			}
+		    System.arraycopy(data, i, tmp, tmp.length-lenLastBlock, lenLastBlock);
+		    tmp[0]=(byte) (tmp.length-lenLastBlock+1);
+		    
+//		    if(tmp.length<127){
+//		    	byte[] padding=new byte[127];
+//		    	
+//		    	System.arraycopy(tmp, 0, padding, 0, tmp.length);
+//		    	padding[tmp.length]=1;
+//		    	if(tmp.length==127){
+//		    		tmp=new byte[padding.length*2];
+//		    	}else{
+//		    		tmp=new byte[padding.length];
+//		    	}
+//		    	System.arraycopy(padding, 0, tmp, 0, padding.length);
+//		    }
 		    tmp=toByteArray((toBigInt(tmp).modPow(publicKey.getE(), publicKey.getN())));
-		    System.arraycopy(tmp, 0, result, i, tmp.length);
+		    for (int j = 0; j < tmp.length; j++) {
+				al.add(tmp[j]);
+			}
+		}
+		result=new byte[al.size()];
+		for (int i = 0; i < al.size(); i++) {
+			result[i]=al.get(i);
 		}
 		System.out.println(Arrays.toString(data));
 		System.out.println(Arrays.toString(result));
@@ -71,9 +93,36 @@ public class RSAImpl implements RSA {
 
 	@Override
 	public byte[] decrypt(byte[] data) {
-		byte[] result=toByteArray((toBigInt(data).modPow(privateKey.getD(), privateKey.getN())));
+		ArrayList<Byte> al=new ArrayList<>();
+		byte[] result;
+		for (int i = 0; i < data.length; i+=128) {
+			byte[] tmp = new byte[128];
+		    System.arraycopy(data, i, tmp, 0, tmp.length);
+//		    if(tmp[tmp.length-1]==0){
+//		    	int idx=tmp.length-1;
+//		    	while(tmp[idx]==0){
+//		    		idx--;
+//		    		if(idx<0)
+//		    			break;
+//		    	}
+//		    	if(idx>=0&&tmp[idx]==1){
+//		    		byte[] nopadding=new byte[idx];
+//		    		System.arraycopy(tmp, 0, nopadding, 0, nopadding.length);
+//		    		tmp=nopadding;
+//		    	}
+//		    }
+		    tmp=toByteArray((toBigInt(tmp).modPow(privateKey.getD(), privateKey.getN())));
+		    
+		    for (int j = 0; j < tmp.length; j++) {
+				al.add(tmp[j]);
+			}
+		}
+		result=new byte[al.size()];
+		for (int i = 0; i < al.size(); i++) {
+			result[i]=al.get(i);
+		}
 		System.out.println(Arrays.toString(data));
-		System.out.println(Arrays.toString(result));		
+		System.out.println(Arrays.toString(result));
 		return result;
 	}
 
